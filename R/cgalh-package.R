e78482a69f99770fee0e6c34c405de586d9a26ae
Ahmadou Dicko <mail@ahmadoudicko.com>
2020-06-07 15:00:42 +0000

    Update and change package name

---
+++ R/cgalh-package.R
@@ -0,0 +1,10 @@
+#' Provides CGAL 'C++' header-only library.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Include a dependency to cgalh inside a cpp file
+#' // [[Rcpp::depends(cgalh)]]
+#' include <Rcpp.h>
+#' }
+"_PACKAGE"
